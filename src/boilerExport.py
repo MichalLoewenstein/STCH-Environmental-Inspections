78da59da6ec1d62ba1c2c9b3c761f92ae899e96d
Shaliyehsabou <t.shaliyehsabou@shell.com>
2026-06-03 10:10:44 +0100

    worked with flow to get correct excel columns

--- src/boilerExport.py
+++ src/boilerExport.py
@@ -7,9 +7,8 @@ def generate_boilerExcel(form_data):
     print(form_data)
     # ✅ Define consistent column order (important!)
     columns = [
-       "Operator 1", "Operator 2", "Operator 3", "Operator 4", "Email", "Phone", 
-        "Boiler Number", "Date",  "Time", "Water Level 1",
-        "Water Level 2", "Water Level 3", "Water Level 4",
+       "Operator", "Email", "Phone", 
+        "Boiler Number", "Date",  "Time", "Water Level",
         "Blow Down Water Column", "Blow Down Sight Glass", "Blow Down Low Water Cut Out", 
         "Bottom Blow Boiler", "Checked Burner Ring For Proper Flame Pattern", "Checked Excess Oxygen For Proper Level",
         "Checked For Excess Combustibles", "Visually Checked Entire Boiler", "Visible Emissions" , 
@@ -18,19 +17,13 @@ def generate_boilerExcel(form_data):
 
     # ✅ Convert incoming form data keys to match column names
     data = {
-        "Operator 1": form_data.get("operator1"),
-        "Operator 2": form_data.get("operator2"),
-        "Operator 3": form_data.get("operator3"),
-        "Operator 4": form_data.get("operator4"),
+        "Operator": form_data.get("operator"),
         "Email": form_data.get("email"),
         "Phone": form_data.get("phone"),
         "Boiler Number": form_data.get("boilerNumber"),
         "Date": form_data.get("date"),
         "Time": form_data.get("time"),
-        "Water Level 1": form_data.get("check_water_level"),
-        "Water Level 2": form_data.get("waterlevel2"),
-        "Water Level 3": form_data.get("waterlevel3"),
-        "Water Level 4": form_data.get("waterlevel4"),
+        "Water Level": form_data.get("check_water_level"),
         "Blow Down Water Column": form_data.get("blowDownWaterColumn"), 
         "Blow Down Sight Glass": form_data.get("blowDownSightGlass"), 
         "Blow Down Low Water Cut Out": form_data.get("blowDownLowWaterCutOut"), 
