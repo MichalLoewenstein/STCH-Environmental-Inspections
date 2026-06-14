9f1b1ca8c6dec27de4d2f1aa2a7019895a6163fb
Nechama T <nechama.taurog@shell.com>
2026-06-14 10:07:50 +0100

    Manufacturer, Model, Tier and Fuel selections for Portable Engine

--- static/JS/portable-engine.js
+++ static/JS/portable-engine.js
@@ -1,17 +1,41 @@
 document.addEventListener("DOMContentLoaded", function () {
-function calculateTotal() {
+
+    // Time Calculation //
     const initial = parseFloat(document.getElementById("initialMeterRead").value) || 0;
     const final = parseFloat(document.getElementById("finalMeterRead").value) || 0;
 
-    if (final >= initial) {
-        const total = final - initial;
-        document.getElementById("total_time").value = total.toFixed(1);
-    } else {
-        document.getElementById("total_time").value = "Invalid";
-    }
-}
+    function calculateTotal() {
 
-// Trigger calculation when values change
-document.getElementById("initialMeterRead").addEventListener("input", calculateTotal);
-document.getElementById("finalMeterRead").addEventListener("input", calculateTotal);
+        if (final >= initial) {
+            const total = final - initial;
+            document.getElementById("total_time").value = total.toFixed(1);
+        } else {
+            document.getElementById("total_time").value = "Invalid";
+        }
+    }
+    // Trigger calculation when values change
+    document.getElementById("initialMeterRead").addEventListener("input", calculateTotal);
+    document.getElementById("finalMeterRead").addEventListener("input", calculateTotal);
+
+    // On-Site Status //
+    const departureDate = document.getElementById("departureDate");
+    const onsiteStatus = document.getElementById("onsiteStatus");
+
+    function updateStatus() {
+
+        if (departureDate.value) {
+            // If there is a date → Off-Site
+            onsiteStatus.value = "status1";
+        } else {
+            // If empty → On-Site
+            onsiteStatus.value = "status2";
+        }
+    }
+
+    // Run on page load
+    updateStatus();
+
+    // Run whenever the date changes
+    departureDate.addEventListener("change", updateStatus);
+
 })
