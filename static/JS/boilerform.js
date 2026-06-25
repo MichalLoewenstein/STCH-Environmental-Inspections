e2bf43dde1123b1432272a9a1ca23c09ecf126b2
Lowenstein <michal.lowenstein@shell.com>
2026-06-25 14:28:55 +0100

    bolier updates

--- static/JS/boilerform.js
+++ static/JS/boilerform.js
@@ -1,11 +1,27 @@
-document.querySelector("form").addEventListener("submit", function () {
+document.addEventListener("DOMContentLoaded", function () {
   const toggles = document.querySelectorAll('input[type="checkbox"]');
 
   toggles.forEach(toggle => {
-    const hidden = document.querySelector(
-      `input[name="${toggle.name}_hidden"]`
-    );
+    // מוצא את ה-hidden לפי שם ה-checkbox בלי _cb
+    const baseName = toggle.name.replace("_cb", "");
+    const hidden = document.querySelector(`input[name="${baseName}"]`);
 
+    if (hidden) {
+      hidden.value = "OFF"; // ברירת מחדל
+
+      toggle.addEventListener("change", function () {
+        hidden.value = toggle.checked ? "ON" : "OFF";
+        console.log(toggle.name,"->",hidden.value)
+      });
+    }
+  });
+});
+
+document.querySelector("form").addEventListener("submit", function () {
+  const toggles = document.querySelectorAll('input[type="checkbox"]');
+  toggles.forEach(toggle => {
+    const baseName = toggle.name.replace("_cb", "");
+    const hidden = document.querySelector(`input[name="${baseName}"]`);
     if (hidden) {
       hidden.value = toggle.checked ? "ON" : "OFF";
     }
